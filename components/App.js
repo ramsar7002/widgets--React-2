702c55de7d3ee11516f92edaac4a59c538360562
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-07-29 14:21:07 +0300

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Accordion from './Accordion';
+
+const App = () => {
+  const items = [
+    {
+      title: 'What is React?',
+      content: 'React is a front end JavaScript framework',
+    },
+
+    {
+      title: 'Why use React?',
+      content: 'React is a favorite JS libary among engineers',
+    },
+
+    {
+      title: 'How do you use React?',
+      content: 'You use React by creating components',
+    },
+  ];
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
+};
+
+export default App;
